fa1ae58bb57071aa1d652ef95e8b44dc2ac5e1f8
Andres Martinez <21141606+andresscode@users.noreply.github.com>
2026-03-08 13:13:39 -0500

    build(vite): split into manual chunks to keep them under 500kb

--- vite.config.ts
+++ vite.config.ts
@@ -11,4 +11,14 @@ export default defineConfig({
       '@': path.resolve(__dirname, './src'),
     },
   },
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ['react', 'react-dom', 'react-router'],
+          ui: ['react-day-picker', 'date-fns', 'zod', 'cmdk'],
+        },
+      },
+    },
+  },
 })
