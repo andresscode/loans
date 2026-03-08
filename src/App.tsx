e9feaddab1bda5777f2ac4e2bb51b274c2bfd441
Andres Martinez <21141606+andresscode@users.noreply.github.com>
2026-03-07 22:35:29 -0500

    chore: install and configure shadcn

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,9 @@
+import "./global.css";
+
 export default function App() {
-  return <div>
-    <h1>Hello, World!</h1>
-  </div>
+  return (
+    <div className="min-h-svh flex items-center justify-center">
+      <h1 className="text-4xl font-medium">Hello, World!</h1>
+    </div>
+  );
 }
