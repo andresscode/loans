93ccd4a30867253259c8f5d03104b731e0ba8677
Andres Martinez <21141606+andresscode@users.noreply.github.com>
2026-03-07 22:24:30 -0500

    build: init electron app with vite and react

---
+++ src/main.ts
@@ -0,0 +1,28 @@
+import { app, BrowserWindow } from 'electron';
+import * as path from 'path';
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      preload: path.join(__dirname, 'preload.js'),
+    },
+  });
+
+  if (!app.isPackaged) {
+    win.loadURL('http://localhost:5173');
+  } else {
+    win.loadFile(path.join(__dirname, '../dist/index.html'));
+  }
+}
+
+app.whenReady().then(createWindow);
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') app.quit();
+});
+
+app.on('activate', () => {
+  if (BrowserWindow.getAllWindows().length === 0) createWindow();
+});
